c99be795e9f338bde68890496898c2cfc03bffc8
nour-alshikh <nooralshikh30@gmail.com>
2024-12-08 14:21:45 +0200

    test

--- app/Http/Controllers/Admin/ProductClassController.php
+++ app/Http/Controllers/Admin/ProductClassController.php
@@ -178,9 +178,10 @@ class ProductClassController extends Controller
                 $image = rand(1, 1500) . "_image." . $extention;
                 $filePath = public_path($image);
 
-                dd($filePath);
-                //
+
                 $fp = file_put_contents($filePath, base64_decode(explode(",", $request->image)[1]));
+
+                dd($fp);
                 $class->addMedia($filePath)->toMediaCollection('product_class');
 
                 // $class->addMediaFromDisk($request->input('uploaded_image_name'), 'temp')->toMediaCollection('product_class');
